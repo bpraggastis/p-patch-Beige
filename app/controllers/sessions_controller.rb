e3c59a4a6e05f292975f96642abc1d346684a42d
Brenda Praggastis <bpraggastis@gmail.com>
2014-12-04 10:35:08 -0800

    removed redundant security checks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :create
-  skip_before_filter :logged_in, only: :create
+
 
   #I don't believe we need the above two lines.
   # see: http://stackoverflow.com/questions/1730377/receive-post-from-external-form
