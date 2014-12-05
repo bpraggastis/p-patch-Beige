040d1083580bb33939cb3e16bcf0ef9fe28d3943
Bri Dotson <bridotson@gmail.com>
2014-12-05 14:34:53 -0800

    add article create method and private article_params method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,19 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
