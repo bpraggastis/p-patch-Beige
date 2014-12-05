f7d31a7005e560479431d023f10af212bbecc54c
Bri Dotson <bridotson@gmail.com>
2014-12-05 15:20:36 -0800

    add rspec test for latest_ten method and add method to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
-
   has_many :comments
   belongs_to :user
+
+  def self.latest_ten
+    Article.order(:created_at).first(10)
+  end
 
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 require 'spec_helper'
 
-describe "Article" do
-  
+describe Article do
+  describe ".latest_ten" do
+    it 'returns the most recent ten or less articles' do
+      expect(Article.latest_ten.count).to be <= 10
+    end
+  end
+
 end
