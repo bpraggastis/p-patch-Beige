7a93eb5d195ccfc1a1c3fcd060ea0db5a4c8a501
Bri Dotson <bridotson@gmail.com>
2014-12-05 15:39:54 -0800

    add article content to news feed on home page. update tests and methods for more control in the controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,12 +2,12 @@ class Article < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  def self.latest_ten
-    Article.order(:created_at).first(10)
+  def self.latest(number)
+    Article.order(created_at: :desc).first(number)
   end
 
-  def content_snippet
-    content.split(" ").first(50).join(" ")
+  def content_snippet(number)
+    content.split(" ").first(number).join(" ")
   end
 
 end
