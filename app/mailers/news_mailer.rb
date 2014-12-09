fd5ccd237418b2d5e91261cdf119f80d0027caa8
Bri Dotson <bridotson@gmail.com>
2014-12-09 09:46:44 -0800

    add welcome method to NewsMailer

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -1,3 +1,9 @@
 class NewsMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "bridotson@gmail.com"
+
+  def welcome(user_id)
+    @user = User.find(user_id)
+    mail(to: "bookis.smuin+club@gmail.com", subject: "Welcome to Whirled Peas")
+  end
+
 end
