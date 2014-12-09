08a50b5c93c8fb0a7b9a5edac9790c6e737d3543
Bri Dotson <bridotson@gmail.com>
2014-12-09 09:53:54 -0800

    edit development.rb to use gmail, edit welcome method in NewsMailer to be simple to test that it works

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -1,8 +1,8 @@
 class NewsMailer < ActionMailer::Base
   default from: "bridotson@gmail.com"
 
-  def welcome(user_id)
-    @user = User.find(user_id)
+  def welcome
+    @user = User.first
     mail(to: "bookis.smuin+club@gmail.com", subject: "Welcome to Whirled Peas News")
   end
 
