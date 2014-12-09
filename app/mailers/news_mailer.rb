2dfc055c80303190c5495bb1f9ee6f2ff62e0627
Bri Dotson <bridotson@gmail.com>
2014-12-09 10:20:42 -0800

    add code to send welcome email from newsmailer when a new account is created

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -1,8 +1,8 @@
 class NewsMailer < ActionMailer::Base
   default from: "bridotson@gmail.com"
 
-  def welcome
-    @user = User.first
+  def welcome(user_id)
+    @user = User.find(user_id)
     mail(to: @user.email, subject: "Welcome to Whirled Peas News")
   end
 
